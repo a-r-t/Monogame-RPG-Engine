157498297b0f7e51e13f9771d61c403f81495ada
a-r-t <alexthimineur@gmail.com>
2025-01-04 01:35:33 -0500

    make generic map entity change visibility script action

---
+++ Monogame-RPG-Engine/src/Engine/Scene/ScriptCore/ScriptActions/MapEntityChangeVisibilityScriptAction.cs
@@ -0,0 +1,51 @@
+﻿using Engine.Scene;
+using Engine.Scene.EntitiesCore;
+using Engine.Scene.ScriptCore;
+using Engine.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Scene.ScriptCore.ScriptActions
+{
+    public class MapEntityChangeVisibilityScriptAction : ScriptAction
+    {
+        protected MapEntity entityToChange;
+        protected Visibility visibility;
+
+        public MapEntityChangeVisibilityScriptAction(MapEntity entity, Visibility visibility)
+        {
+            this.entityToChange = entity;
+            this.visibility = visibility;
+        }
+
+        public MapEntityChangeVisibilityScriptAction(Visibility visibility)
+        {
+            this.visibility = visibility;
+        }
+
+        public override void Setup()
+        {
+            if (this.entity == null)
+            {
+  
+                throw new Exception("No entity specified!");
+            }
+        }
+
+        public override ScriptState Execute()
+        {
+            if (visibility == Visibility.VISIBLE)
+            {
+                entity.IsHidden = false;
+            }
+            else
+            {
+                entity.IsHidden = true;
+            }
+            return ScriptState.COMPLETED;
+        }
+    }
+}
